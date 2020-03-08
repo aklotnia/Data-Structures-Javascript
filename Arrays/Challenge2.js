9efc30eb69d5a1b1b2eaf5bd666f4b9257c211f2
Aaron Klotnia <aklotnia@gmail.com>
2020-03-07 19:56:12 -0500

    Challenge2 Added

---
+++ Arrays/Challenge2.js
@@ -0,0 +1,3 @@
+// Challenge 2: Merge Two Sorted Arrays
+// Given two sorted arrays, merge them into one array that is sorted. 
+// Implement the solution in JavaScript to see if your code runs successfully!
